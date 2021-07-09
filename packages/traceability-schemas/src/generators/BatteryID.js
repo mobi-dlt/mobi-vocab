7cd1775c075838d0c095b8bab3a70893b1b99db9
Maying(Matt) Shi <himatt93@gmail.com>
2021-07-09 03:03:50 -0700

    updated Battery ID using Product schema

--- packages/traceability-schemas/src/generators/BatteryID.js
+++ packages/traceability-schemas/src/generators/BatteryID.js
@@ -1,18 +1,18 @@
 const faker = require('faker');
 
 const { getEntity } = require('./Entity');
+const { getProduct } = require('./Product');
 
 const getBatteryID = () => {
-    const manufacturer = getEntity();
-    delete manufacturer['@context'];
-    manufacturer.name = 'Panasonic';
+    const ProductInfo = getProduct();
+    delete ProductInfo['@context'];
 
     const example = {
         '@context': ['https://mobinet.io/schemas/v1'],
         type: 'BatteryID',
         VID: 'did:mobi:c276e12ec21ebfeb1f712ebc6f',
         BatteryID: 'did:mobi:ebfeb1f712ebc6f1c276e12ec21',
-        manufacturer,
+        ProductInfo,
         model: 'Panasonic EV',
         serialNumber: 'H1591BO0732A',
     };
