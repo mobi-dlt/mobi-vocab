aa5c98507b5648c7f655d99eae9eaf0e231d90d9
Maying(Matt) Shi <himatt93@gmail.com>
2021-07-20 17:25:14 -0700

    added vid batteryid w/ ref

---
+++ packages/traceability-schemas/src/generators/VID.js
@@ -0,0 +1,21 @@
+const faker = require('faker');
+const { getEntity } = require('./Entity');
+
+const getVID = () => {
+    const manufacturer = getEntity();
+    delete manufacturer['@context'];
+
+    const example = {
+        '@context': ['https://w3id.org/traceability/v1'],
+        type: 'VID',
+        vid: 'did:mobi:c276e12ec21ebfeb1f712ebc6f',
+        manufacturer,
+        make: 'Ford',
+        model: 'Mach_E',
+        vin: 'JTERU5JR7M5861170',
+
+    };
+  return example;
+};
+
+module.exports = { getVID };
