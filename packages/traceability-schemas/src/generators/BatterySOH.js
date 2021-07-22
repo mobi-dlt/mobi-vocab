12369b14ef8094e02cddfe0b77fdf3a21f2c2eec
Maying(Matt) Shi <himatt93@gmail.com>
2021-07-21 17:56:34 -0700

    updated batterySOH with extra element

--- packages/traceability-schemas/src/generators/BatterySOH.js
+++ packages/traceability-schemas/src/generators/BatterySOH.js
@@ -9,11 +9,17 @@ const getBatterySOH = () => {
     const example = {
         '@context': ['https://w3id.org/traceability/v1'],
         type: 'BatterySOH',
+        sohCertID: 'idnumber',
+        sohCertIssuerID: 'idnumber',
         batteryid: batteryIdentifier,
         previousSOHVal: '95',
         previousSOHTimeStamp: '2020-5-17',
         newSOHVal: '94',
-        newSOHTimeStamp: '2020-10-17'
+        newSOHTimeStamp: '2020-10-17',
+        chargeCycles: '30',
+        maxCapacity: '87',
+        impedance: '70',
+        batteryAge: '1.2'
     };
     return example;
 };
