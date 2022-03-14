9949bf3e5192267fd6c763c2f20910f053b6ca4f
Parth Bhatt <pbjm2493@gmail.com>
2022-03-14 16:46:40 -0700

    fixed partsReplacement

--- packages/traceability-schemas/src/generators/VcSchemaForPartsReplacementReport.js
+++ packages/traceability-schemas/src/generators/VcSchemaForPartsReplacementReport.js
@@ -9,7 +9,7 @@ const getVcSchemaForPartsReplacementReport = () => {
         geolocation: 'test',
         estimateNumber: 'test',
         invoiceNumber: 'test',
-        // // partType: 'test',
+        partType: 'test',
         oldPartNumber: 'test',
         oldPartSerialNumber: 'test',
         oldPartManufacturer: 'test',
@@ -17,7 +17,7 @@ const getVcSchemaForPartsReplacementReport = () => {
         finalPartNumber: 'test',
         finalPartSerialNumber: 'test',
         finalPartManufacturer: 'test',
-        // finalPartCondition: 'test',
+        finalPartCondition: 'test',
         partRepairedOrReplaced: 'test',
         aftermarketLikeNewPart: 'test',
         reasonForReplacement: 'test',
@@ -29,21 +29,3 @@ const getVcSchemaForPartsReplacementReport = () => {
 };
 
 module.exports = { getVcSchemaForPartsReplacementReport };
-
-/**
- *
- *  "partType": {
-            "$comment": "{\"term\": \"partType\", \"@id\": \"https://schema.org/additionalType\"}",
-            "title": "partType",
-            "description": "the type of part involved in the replacement",
-            "type": "string"
-        },
-
-         "finalPartCondition": {
-            "$comment": "{\"term\": \"finalPartCondition\", \"@id\": \"https://schema.org/additionalProperty\"}",
-            "title": "finalPartCondition",
-            "description": "the condition of the new part installed into the vehicle",
-            "type": "string"
-        },
-
- */
